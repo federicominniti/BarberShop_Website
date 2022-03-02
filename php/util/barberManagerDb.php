dea0c120f9e1e196ada66dbc0fb7a35bf0a24d95
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-03-02 14:29:26 +0100

    Update barberManagerDb.php

--- php/util/barberManagerDb.php
+++ php/util/barberManagerDb.php
@@ -1,5 +1,5 @@
 <?php
-	require_once __DIR__ . "/../config.php";
+    require_once __DIR__ . "/../config.php";
     require_once DIR_UTIL . "barbershopDbManager.php";
  	
     //Ricerche effettuate sul database
